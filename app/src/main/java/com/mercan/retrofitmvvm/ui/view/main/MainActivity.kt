d13f7bf618b0536dd406db1b0b5eab8676e137af
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-07 16:53:18 +0300

    Project structure completed.

--- app/src/main/java/com/mercan/retrofitmvvm/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/mercan/retrofitmvvm/ui/view/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.mercan.retrofitmvvm
+package com.mercan.retrofitmvvm.ui.view.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mercan.retrofitmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
