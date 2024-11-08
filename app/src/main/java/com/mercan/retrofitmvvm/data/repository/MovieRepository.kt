a9bb348e8c386997cbe917692a65cd12fe6f92f8
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-08 18:48:43 +0300

    Created MovieRepository for Retrofit.

---
+++ app/src/main/java/com/mercan/retrofitmvvm/data/repository/MovieRepository.kt
@@ -0,0 +1,18 @@
+package com.mercan.retrofitmvvm.data.repository
+
+import com.mercan.retrofitmvvm.core.Constants
+import com.mercan.retrofitmvvm.data.remote.MovieService
+
+class MovieRepository(private val movieService: MovieService) {
+    suspend fun getPopularMovies() =
+        movieService.getMovieList(apiRoute = Constants.POPULAR)
+
+    suspend fun getNowPlayingMovies() =
+        movieService.getMovieList(apiRoute = Constants.NOW_PLAYING)
+
+    suspend fun getTopRatedMovies() =
+        movieService.getMovieList(apiRoute = Constants.TOP_RATED)
+
+    suspend fun getUpcomingMovies() =
+        movieService.getMovieList(apiRoute = Constants.UPCOMING)
+}
