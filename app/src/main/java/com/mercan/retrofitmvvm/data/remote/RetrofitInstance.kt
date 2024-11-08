804f7b2c15ae6fa1f6ed6a705e25ed1a1f2300cd
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-08 18:48:29 +0300

    Created RetrofitInstance for Retrofit.

---
+++ app/src/main/java/com/mercan/retrofitmvvm/data/remote/RetrofitInstance.kt
@@ -0,0 +1,15 @@
+package com.mercan.retrofitmvvm.data.remote
+
+import com.mercan.retrofitmvvm.core.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    val movieService: MovieService by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MovieService::class.java)
+    }
+}
