b3e7abf02fa4e245f03e17760e280bd15adb8dc7
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-08 18:46:51 +0300

    Added new constants for movies.

--- app/src/main/java/com/mercan/retrofitmvvm/core/Constants.kt
+++ app/src/main/java/com/mercan/retrofitmvvm/core/Constants.kt
@@ -3,5 +3,11 @@ package com.mercan.retrofitmvvm.core
 import com.mercan.retrofitmvvm.BuildConfig
 
 object Constants {
-    val apiKey: String = BuildConfig.API_KEY
+    const val BASE_URL: String = BuildConfig.BASE_URL
+    const val API_KEY: String = BuildConfig.API_KEY
+
+    const val NOW_PLAYING = "now_playing"
+    const val POPULAR = "popular"
+    const val TOP_RATED = "top_rated"
+    const val UPCOMING = "upcoming"
 }
