bbc60c92dfdd456cb24840ad75450d11f9916757
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-16 18:32:29 +0300

    Renamed MovieTrailerRepository to MovieTrailersRepository

--- app/src/main/java/com/mercan/retrofitmvvm/data/repository/MovieTrailersRepository.kt
+++ app/src/main/java/com/mercan/retrofitmvvm/data/repository/MovieTrailersRepository.kt
@@ -2,7 +2,7 @@ package com.mercan.retrofitmvvm.data.repository
 
 import com.mercan.retrofitmvvm.data.remote.MovieTrailersService
 
-class MovieTrailerRepository(private val movieTrailersService: MovieTrailersService) {
+class MovieTrailersRepository(private val movieTrailersService: MovieTrailersService) {
     suspend fun getMovieTrailersById(id: Long) =
         movieTrailersService.getMovieTrailersById(id = id)
 }
