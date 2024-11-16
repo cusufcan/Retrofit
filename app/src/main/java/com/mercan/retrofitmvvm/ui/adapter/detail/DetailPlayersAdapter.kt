9f035d371a868f04f04fdaa55049751f7c33efba
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-16 14:00:22 +0300

    Completed DetailPlayersAdapter and DetailPlayersViewHolder

--- app/src/main/java/com/mercan/retrofitmvvm/ui/adapter/detail/DetailPlayersAdapter.kt
+++ app/src/main/java/com/mercan/retrofitmvvm/ui/adapter/detail/DetailPlayersAdapter.kt
@@ -3,9 +3,10 @@ package com.mercan.retrofitmvvm.ui.adapter.detail
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.mercan.retrofitmvvm.data.model.credits.Cast
 import com.mercan.retrofitmvvm.databinding.PlayerCardItemBinding
 
-class DetailPlayersAdapter(private val players: List<String>) :
+class DetailPlayersAdapter(private val cast: List<Cast>?) :
     RecyclerView.Adapter<DetailPlayersViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailPlayersViewHolder {
         val binding = PlayerCardItemBinding.inflate(
@@ -15,9 +16,9 @@ class DetailPlayersAdapter(private val players: List<String>) :
     }
 
     override fun onBindViewHolder(holder: DetailPlayersViewHolder, position: Int) {
-        val player = players[position]
-        holder.bind(player, player, player)
+        val player = cast?.get(position)
+        holder.bind(player)
     }
 
-    override fun getItemCount() = players.size
+    override fun getItemCount() = cast?.size ?: 0
 }
--- app/src/main/java/com/mercan/retrofitmvvm/ui/adapter/detail/DetailPlayersViewHolder.kt
+++ app/src/main/java/com/mercan/retrofitmvvm/ui/adapter/detail/DetailPlayersViewHolder.kt
@@ -1,18 +1,18 @@
 package com.mercan.retrofitmvvm.ui.adapter.detail
 
 import androidx.recyclerview.widget.RecyclerView
+import com.mercan.retrofitmvvm.core.Constants
+import com.mercan.retrofitmvvm.data.model.credits.Cast
 import com.mercan.retrofitmvvm.databinding.PlayerCardItemBinding
 import com.squareup.picasso.Picasso
 
 class DetailPlayersViewHolder(private val binding: PlayerCardItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
-    fun bind(
-        imagePath: String,
-        name: String,
-        character: String,
-    ) {
+    fun bind(player: Cast?) {
+        val imagePath = Constants.IMAGE_BASE_URL + player?.profilePath
+
         Picasso.get().load(imagePath).into(binding.portraitImageView)
-        binding.realNameTextView.text = name
-        binding.realNameTextView.text = character
+        binding.realNameTextView.text = player?.originalName
+        binding.characterNameTextView.text = player?.character
     }
 }
