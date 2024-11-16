07f4297f7b4d4062402bad270452c1efacd13d65
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-16 17:32:47 +0300

    Created MovieRecommendationsViewModel

---
+++ app/src/main/java/com/mercan/retrofitmvvm/ui/viewmodel/MovieRecommendationsViewModel.kt
@@ -0,0 +1,32 @@
+package com.mercan.retrofitmvvm.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mercan.retrofitmvvm.data.model.MovieList
+import com.mercan.retrofitmvvm.data.remote.RetrofitInstance
+import com.mercan.retrofitmvvm.data.repository.MovieRecommendationsRepository
+import kotlinx.coroutines.launch
+
+class MovieRecommendationsViewModel : ViewModel() {
+    private val movieRecommendationsRepository =
+        MovieRecommendationsRepository(RetrofitInstance.movieRecommendationsService)
+
+    val movieRecommendations = MutableLiveData<MovieList>()
+    val movieRecommendationsLoading = MutableLiveData<Boolean>()
+
+    val errorMessage = MutableLiveData<String>()
+
+    fun getRecommendationsById(id: Long) = viewModelScope.launch {
+        movieRecommendationsLoading.postValue(true)
+        val response = movieRecommendationsRepository.getRecommendationsById(id = id)
+        if (response.isSuccessful) {
+            println(response.body())
+            movieRecommendations.postValue(response.body())
+        } else {
+            println(response.message())
+            errorMessage.postValue(response.message())
+        }
+        movieRecommendationsLoading.postValue(false)
+    }
+}
