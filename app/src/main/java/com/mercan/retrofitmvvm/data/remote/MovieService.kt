a124a75b86220df2eb1e3746395eaeb730ed313f
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-19 18:19:00 +0300

    Added page query to MovieService for paging

--- app/src/main/java/com/mercan/retrofitmvvm/data/remote/MovieService.kt
+++ app/src/main/java/com/mercan/retrofitmvvm/data/remote/MovieService.kt
@@ -7,12 +7,14 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MovieService {
     @GET("movie/{route}")
     suspend fun getMovieList(
         @Header("Authorization") token: String = Constants.API_KEY,
         @Path("route") apiRoute: String,
+        @Query("page") page: Int = 1,
     ): Response<MovieList>
 
     @GET("genre/movie/list")
