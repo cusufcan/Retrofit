b5861ab26bdd05d00ae4036037392a7ee93fb9cb
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-20 18:59:08 +0300

    Changed RecyclerView.Adapter to ListAdapter and added differCallback to HomeSliderAdapter

--- app/src/main/java/com/mercan/retrofitmvvm/ui/adapter/homeslider/HomeSliderAdapter.kt
+++ app/src/main/java/com/mercan/retrofitmvvm/ui/adapter/homeslider/HomeSliderAdapter.kt
@@ -3,10 +3,12 @@ package com.mercan.retrofitmvvm.ui.adapter.homeslider
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.mercan.retrofitmvvm.core.Constants
 import com.mercan.retrofitmvvm.core.Route
 import com.mercan.retrofitmvvm.data.model.GenreList
+import com.mercan.retrofitmvvm.data.model.Movie
 import com.mercan.retrofitmvvm.data.model.MovieList
 import com.mercan.retrofitmvvm.databinding.MovieCardBigBinding
 import com.mercan.retrofitmvvm.ui.bottomsheet.HomeBottomSheetDialog
@@ -15,7 +17,7 @@ class HomeSliderAdapter(
     private val fragmentManager: FragmentManager,
     private val movies: MovieList,
     private val genres: GenreList,
-) : RecyclerView.Adapter<HomeSliderViewHolder>() {
+) : ListAdapter<Movie, HomeSliderViewHolder>(differCallback) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeSliderViewHolder {
         val binding = MovieCardBigBinding.inflate(
             LayoutInflater.from(parent.context), parent, false
@@ -31,6 +33,16 @@ class HomeSliderAdapter(
                 it,
                 Route.HOME,
             ).show(fragmentManager, Constants.NORMAL_TAG)
+        }
+    }
+
+    companion object {
+        private val differCallback = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie) =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie) =
+                oldItem == newItem
         }
     }
 }
